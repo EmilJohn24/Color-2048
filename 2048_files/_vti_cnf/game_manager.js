5e43875e54e1b6031f8221eb77014977e4fdd892
EJL242000 <emiljohn24@gmail.com>
2014-05-08 10:51:35 +0800

    Changed the stylesheet
    Changed the css of org. 2048

---
+++ 2048_files/_vti_cnf/game_manager.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|08 May 2014 01:05:08 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|2048.htm
+vti_author:SR|ejl\\ejlfarm
+vti_modifiedby:SR|ejl\\ejlfarm
+vti_timecreated:TR|02 Apr 2014 12:35:47 -0000
+vti_syncofs_localhost\\c\:\\users\\ejlfarm\\desktop\\ej\\host 2048 parody/c\:/users/ejlfarm/desktop/ej/host 2048 parody:TW|03 Apr 2014 06:51:40 -0000
+vti_syncwith_localhost\\c\:\\users\\ejlfarm\\desktop\\ej\\host 2048 parody/c\:/users/ejlfarm/desktop/ej/host 2048 parody:TW|02 Apr 2014 12:35:47 -0000
+vti_nexttolasttimemodified:TW|07 Apr 2014 12:59:53 -0000
+vti_cacheddtm:TX|08 May 2014 01:05:08 -0000
+vti_filesize:IR|7962
---
+++ 2048_files/_vti_cnf/html_actuator.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Apr 2014 12:16:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|2048.htm
+vti_author:SR|ejl\\ejlfarm
+vti_modifiedby:SR|ejl\\ejlfarm
+vti_timecreated:TR|02 Apr 2014 12:15:58 -0000
+vti_syncofs_localhost\\c\:\\users\\ejlfarm\\desktop\\ej\\host 2048 parody/c\:/users/ejlfarm/desktop/ej/host 2048 parody:TW|03 Apr 2014 06:51:40 -0000
+vti_syncwith_localhost\\c\:\\users\\ejlfarm\\desktop\\ej\\host 2048 parody/c\:/users/ejlfarm/desktop/ej/host 2048 parody:TW|02 Apr 2014 12:15:58 -0000
+vti_cacheddtm:TX|02 Apr 2014 12:16:00 -0000
+vti_filesize:IR|5045
